4e33dcb6f49d3fd45761b45e8026da855cdc1f2c
Ayush <ayushsenapati123@gmail.com>
2021-10-10 20:07:57 +0530

    Added API Keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "f12ccff5";
+  const APP_KEY = ""
+
+  return(
     <div className="App">
-     <h1>Hello Ayush</h1>
+      <h1>Hello Ayush</h1>
     </div>
-  );
+  )
 }
 
 export default App;
