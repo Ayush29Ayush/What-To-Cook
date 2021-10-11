c5029b70b615964a73ce07828d25009febe42789
Ayush <ayushsenapati123@gmail.com>
2021-10-11 13:45:41 +0530

    Fixed issue related to API call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  useEffect(async () => {}, []);
+
+  let app_id = process.env.REACT_APP_CLIENT_ID;
+  let app_key = process.env.REACT_APP_CLIENT_KEY;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+      `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`
     );
+    // Converting response to json for ease
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
   };
 
   return (
