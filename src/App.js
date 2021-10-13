259e757ab92471add54f13c44e6254defcdb01ab
Ayush <ayushsenapati123@gmail.com>
2021-10-13 12:29:18 +0530

    Added ingredients prop

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ const App = () => {
   //! State
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken') // this will be the term used for getting recipes from server and will only be activated when search button is clicked.
 
   //! useEffect
+  // useEffect(() => {
+  //   getRecipes();
+  // }, []);
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
+    // const response = await fetch(
+    //   `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`
+    // );
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`
+      `https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`
     );
     // Converting response to json for ease
     const data = await response.json();
@@ -32,16 +39,22 @@ const App = () => {
 
   const updateSearch = (e) => {
     // Thus e.target.value is the value property of some DOM element, in this case that means the text entered in the search input.
-    console.log(e.target.value)
+    // console.log(e.target.value)
     setSearch(e.target.value);
     // console.log(search)
 
   };
 
+  const getSearch = (e) => {
+    e.preventDefault(); // prevents from refreshing the page on submitting
+    setQuery(search);
+    setSearch(''); // after setting the query , again setting search to blank , since it will be reflected in the UI.
+  }
+
   return (
     <div className="App">
       <h1>Hello Ayush</h1>
-      <form className="search-form">
+      <form className="search-form" onSubmit={getSearch}>
         <input
           type="text"
           className="search-bar"
@@ -59,6 +72,7 @@ const App = () => {
           title={xyzrecipe.recipe.label}
           calories={xyzrecipe.recipe.calories}
           image={xyzrecipe.recipe.image}
+          ingredients={xyzrecipe.recipe.ingredients}
         />
       ))}
     </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 // const Recipe = (props) => {
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div>
       {/* <h1>Title</h1> */}
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map(xyzingredient => (
+          <li>{xyzingredient.text}</li>
+        ))}
+      </ol>
       {/* <p>Calories</p> */}
       <p>{calories}</p>
       <img src={image} alt="" />
