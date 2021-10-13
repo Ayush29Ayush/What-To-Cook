1fd833341060e913464551b142481dd41a1c0439
Ayush <ayushsenapati123@gmail.com>
2021-10-13 13:43:06 +0530

    Added styling for Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,19 +1,20 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 // const Recipe = (props) => {
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       {/* <h1>Title</h1> */}
       <h1>{title}</h1>
       <ol>
-        {ingredients.map(xyzingredient => (
+        {ingredients.map((xyzingredient) => (
           <li>{xyzingredient.text}</li>
         ))}
       </ol>
       {/* <p>Calories</p> */}
       <p>{calories}</p>
-      <img src={image} alt="" />
+      <img src={image} alt="" className={style.image} />
     </div>
   );
 };
